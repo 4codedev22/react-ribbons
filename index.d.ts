957d1db2fbbbf17dc19b2a557f24ef155c249128
phytter <phytter@hotmail.com>
2023-06-08 19:12:50 -0300

    fix: trying fix declare module

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,22 @@
-import React from "react";
 
-export interface RibbonContainerProps {
-  children: React.ReactNode
+declare module "react-ribbons" {
+  import { Component } from 'react'
+
+  export interface RibbonContainerProps {
+    className?: string
+  }
+
+  class RibbonContainer extends Component<RibbonContainerProps, any> { render: () => any }
+  
+  export interface RibbonProps {
+    side: "right" | "left"
+    type: "corner" | "edge"
+    size: "normal" | "large"
+    backgroundColor?: string
+    color?: string
+    fontFamily?: string
+    withStripes?: boolean
+  }
+  
+  class Ribbon extends Component<RibbonProps, any> { render: () => any }
 }
-
-declare class RibbonContainer extends React.Component<RibbonContainerProps, any> { }
-
-export interface RibbonProps {
-  side: "right" | "left"
-  type: "corner" | "edge"
-  size: "normal" | "large"
-  children: React.ReactNode
-  backgroundColor?: string
-  color?: string
-  fontFamily?: string
-  withStripes?: boolean
-}
-
-export class Ribbon extends React.Component<RibbonProps, any> { }
